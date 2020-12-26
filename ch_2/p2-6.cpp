3f983dfb8356d6e7078663ac6e76e804173efd2a
Bogdan Deac <bogdan.p.deac@gmail.com>
2020-12-26 08:19:00 +0200

    add exercise 2-6

---
+++ ch_2/p2-6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int i = 0;
+
+    // Display the numbers from 1 to 10
+    while (i < 10) {
+        i += 1;
+        std::cout << i << std::endl;
+    }
+}
