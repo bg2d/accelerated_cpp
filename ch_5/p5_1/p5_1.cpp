9aa08475817f13c2338307baec95afdade6eb961
Bogdan <bogdan.p.deac@gmail.com>
2021-05-30 15:40:28 +0300

    add exercise 5-1; add a structure that keeps the permutation and the number of rotations needed to obtain it

--- ch_5/p5_1/p5_1.cpp
+++ ch_5/p5_1/p5_1.cpp
@@ -11,6 +11,11 @@ using std::string;
 using std::istream;
 using std::vector;
 
+typedef struct {
+    vector<string> line;
+    int no_rotations;
+} line_t;
+
 istream& read_line(istream& is, vector<string>& line) {
     if (is) {
         line.clear();
@@ -36,38 +41,50 @@ void print_line(const vector<string>& line) {
     cout << endl;
 }
 
-void generate_all_rotations(vector<string>& line, vector<vector<string>>& permutations) {
+void generate_all_rotations(vector<string>& line, vector<line_t>& permutations) {
     vector<string>::size_type no_rotations = 0;
 
+    line_t one_permutation;
+
     // Insert the initial string
-    permutations.push_back(line);
+    one_permutation.line = line;
+    one_permutation.no_rotations = 0;
+    permutations.push_back(one_permutation);
 
     while (no_rotations < line.size()-1) {
         std::rotate(line.begin(), line.begin()+1, line.end());
-        //print_line(line);
-        permutations.push_back(line);
         ++no_rotations;
+        one_permutation.line = line;
+        one_permutation.no_rotations = no_rotations;
+        permutations.push_back(one_permutation);
     }
+}
+
+void display_permuted_index(vector<line_t>& permutations) {
+    cout << "===========================" << endl;
+    auto iter = permutations.begin();
+    while (iter != permutations.end()) {
+        print_line(iter->line);
+        cout << iter->no_rotations << endl;
+
+        ++iter;
+    }
+    cout << "===========================" << endl;
 }
 
 
 int main() {
-    vector<vector<string>> permutations;
+    vector<line_t> permutations;
     vector<string> line;
 
     read_line(cin, line);
 
     generate_all_rotations(line, permutations);
 
-    std::sort(permutations.begin(), permutations.end());
+    // TODO: reimplement this
+    //std::sort(permutations.begin(), permutations.end());
 
-    // Just for debugging
-    cout << "===========================" << endl;
-    auto iter = permutations.begin();
-    while (iter != permutations.end()) {
-        print_line(*iter);
-        ++iter;
-    }
+    display_permuted_index(permutations);
     
     return 0;
 }
